8f5efaa1cc2b097fb8600e66af6d13226e9054d8
thaiphan <thaiphan2712@gmail.com>
2025-07-29 16:48:38 -0500

    Update Lab 8 Part A Test

--- Lab8/Lab8PartATest/src/main/java/lab/AppLab8PartATest.java
+++ Lab8/Lab8PartATest/src/main/java/lab/AppLab8PartATest.java
@@ -3,18 +3,47 @@ package lab;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestClient;
 
 
 @SpringBootApplication
 public class AppLab8PartATest implements CommandLineRunner {
 
-  public static void main(String[] args)  {
+
+  public static void main(String[] args) {
     SpringApplication.run(AppLab8PartATest.class, args);
   }
 
   @Override
   public void run(String... args) throws Exception {
+    RestClient restClient = RestClient.builder()
+        .baseUrl("http://localhost:8080")
+        .build();
 
+    BookDTO bookDTO = restClient.get()
+          .uri("/api/books/111-3-16-2222-0")
+        .retrieve()
+        .body(BookDTO.class);
+    System.out.println(bookDTO);
+
+    BookDTO newBook = new BookDTO("111-3-16-2227-0", "Spring Boot in Action", "Craig Walls", 29.99);
+    bookDTO = restClient
+        .post()
+        .uri("/api/books")
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(newBook)
+        .retrieve()
+        .body(BookDTO.class);
+    System.out.println(bookDTO);
+
+//// add John
+//    Contact johnResponse = restClient.post()
+//        .uri("/contacts")
+//        .contentType(MediaType.APPLICATION_JSON)
+//        .body(new Contact("John", "Doe", "jdoe@acme.com", "6739127563"))
+//        .retrieve()
+//        .body(Contact.class);
   }
 }
 
---
+++ Lab8/Lab8PartATest/src/main/java/lab/BookDTO.java
@@ -0,0 +1,44 @@
+package lab;
+
+
+public class BookDTO {
+
+  private String isbn;
+  private String title;
+  private String author;
+  private double price;
+
+
+  public BookDTO(String isbn, String title, String author, double price) {
+    this.isbn = isbn;
+    this.title = title;
+    this.author = author;
+    this.price = price;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getIsbn() {
+    return isbn;
+  }
+
+  public String getAuthor() {
+    return author;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  @Override
+  public String toString() {
+    return "BookDTO{" +
+        "isbn='" + isbn + '\'' +
+        ", title='" + title + '\'' +
+        ", author='" + author + '\'' +
+        ", price=" + price +
+        '}';
+  }
+}
