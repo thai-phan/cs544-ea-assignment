19b6727f09f034c515cedfa3a5be63a4b9bd481c
thaiphan <thaiphan2712@gmail.com>
2025-08-02 07:55:09 -0500

    update Lab 9 Part A

--- Lab9/Lab9Sender/src/main/java/jms/SpringJmsPersonSenderApplication.java
+++ Lab9/Lab9Sender/src/main/java/jms/SpringJmsPersonSenderApplication.java
@@ -1,24 +1,13 @@
 package jms;
 
-import jakarta.jms.ConnectionFactory;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
-
-import java.util.Optional;
 
 
 @SpringBootApplication
@@ -41,7 +30,7 @@ public class SpringJmsPersonSenderApplication implements CommandLineRunner {
 		String personAsString = objectMapper.writeValueAsString(person);
 
 		System.out.println("Sending a JMS message:" + personAsString);
-		jmsTemplate.convertAndSend("testQueue",personAsString);
+		jmsTemplate.convertAndSend("testTopic",personAsString);
 
 	}
 
