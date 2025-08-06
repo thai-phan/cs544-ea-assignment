93103a37a367bc8aaf595205ceba31a0a4bd3c7a
thaiphan <thaiphan2712@gmail.com>
2025-08-05 23:14:22 -0500

    Add Lab 12 Part C

--- Lab12/WebSecurityProject2/src/main/java/myapplication/security/userservice/SecurityUser.java
+++ Lab12/WebSecurityProject2/src/main/java/myapplication/security/userservice/SecurityUser.java
@@ -1,6 +1,5 @@
 package myapplication.security.userservice;
 
-import myapplication.security.userservice.User;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
